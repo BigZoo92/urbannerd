73b6db229af2e690c72969551dcc8cfae1f6a82c
BigZoo92 <givernaudenzo@gmail.com>
2024-01-03 05:57:21 +0100

    ad duploads and template mail to the build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import router from './routes/routes';
-import { corsOptions, port } from './constant';
-import path from 'path';
-import compression from 'compression';
-import morgan from 'morgan';
-import { PrismaClient } from '@prisma/client';
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/routes";
+import { corsOptions, port } from "./constant";
+import path from "path";
+import compression from "compression";
+import morgan from "morgan";
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
@@ -20,20 +20,20 @@ app.use(express.json());
 
 // MIDDLEWARE
 app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(compression());
 app.use(cors(corsOptions));
 
 // STATIC ASSETS
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 // BACKEND'S HOME
-app.get('/', (_, res) => {
-  res.send('Hello, World!');
+app.get("/", (_, res) => {
+  res.send("Hello, World!");
 });
 
 // ROUTES
-app.use('/api', router);
+app.use("/api", router);
 
 // START THE SERVER
 app.listen(port, () => {
