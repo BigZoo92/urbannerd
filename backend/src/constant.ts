0c55108f378995ca52320192c4678aec308aeaf2
BigZoo92 <givernaudenzo@gmail.com>
2024-01-03 06:11:31 +0100

    get image from uploads

--- backend/src/constant.ts
+++ backend/src/constant.ts
@@ -1,11 +1,11 @@
-import { CorsOptions } from 'cors';
-import multer from 'multer';
-import path from 'path';
-import dotenv from 'dotenv';
+import { CorsOptions } from "cors";
+import multer from "multer";
+import path from "path";
+import dotenv from "dotenv";
 
 dotenv.config();
 // WHITELIST
-export const whitelist = ['http://localhost:3000'];
+export const whitelist = ["http://localhost:3000"];
 
 // CORS
 export const corsOptions: CorsOptions = {
@@ -15,25 +15,26 @@ export const corsOptions: CorsOptions = {
 
 export const port = process.env.PORT || 4000;
 
-export const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+export const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
-export const bddUrl = process.env.DATABASE_URL
+export const bddUrl = process.env.DATABASE_URL;
 
-export const secret = process.env.SESSION_SECRET || 'session_secret_not_found';
+export const secret = process.env.SESSION_SECRET || "session_secret_not_found";
 
-export const jwtToken = process.env.JWT_SECRET || 'jwt_secret_not_found';
+export const jwtToken = process.env.JWT_SECRET || "jwt_secret_not_found";
 
 export const storage = multer.diskStorage({
-  destination: './uploads',
+  destination: "./src/uploads",
   filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + 
-  path.extname(file.originalname));
-  }
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 export const upload = multer({ storage: storage }).fields([
-  { name: 'files', maxCount: 4 },
-  { name: 'pp', maxCount: 1 }, 
-  { name: 'images', maxCount: 4 }, 
-  { name: 'model3D', maxCount: 4 },
+  { name: "files", maxCount: 4 },
+  { name: "pp", maxCount: 1 },
+  { name: "images", maxCount: 4 },
+  { name: "model3D", maxCount: 4 },
 ]);
-
