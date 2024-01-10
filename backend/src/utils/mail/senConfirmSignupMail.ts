d8b28f517efd5725cef6bf1a41c3030d573b5966
BigZoo92 <givernaudenzo@gmail.com>
2024-01-10 19:17:17 +0100

    little commit

--- backend/src/utils/mail/senConfirmSignupMail.ts
+++ backend/src/utils/mail/senConfirmSignupMail.ts
@@ -1,11 +1,11 @@
-import nodemailer from 'nodemailer';
-import jwt from 'jsonwebtoken';
-import { jwtToken } from '../../constant';
-import fs from 'fs';
-import path from 'path';
+import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
+import { jwtToken } from "../../constant";
+import fs from "fs";
+import path from "path";
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
@@ -16,21 +16,21 @@ const transporter = nodemailer.createTransport({
 
 export const sendConfirmSignupMail = async (email: string) => {
   const emailToken = jwt.sign({ email }, jwtToken, {
-    expiresIn: '1d',
+    expiresIn: "1d",
   });
 
-  const emailConfirmationLink = `https://1a51-2001-861-5e60-3110-7074-d229-b5bd-e6b.ngrok-free.app/api/auth/confirmSignup?token=${emailToken}`;
+  const emailConfirmationLink = `https://urbannerd-production.up.railway.app/api/auth/confirmSignup?token=${emailToken}`;
 
   const htmlTemplatePath = path.resolve(
     __dirname,
-    '../../templates/mail/confirmSignup.html',
+    "../../templates/mail/confirmSignup.html"
   );
 
-  const htmlTemplate = fs.readFileSync(htmlTemplatePath, 'utf8');
+  const htmlTemplate = fs.readFileSync(htmlTemplatePath, "utf8");
 
   const emailHTML = htmlTemplate.replace(
-    '{{confirmationLink}}',
-    emailConfirmationLink,
+    "{{confirmationLink}}",
+    emailConfirmationLink
   );
 
   await transporter.sendMail({
@@ -43,21 +43,21 @@ export const sendConfirmSignupMail = async (email: string) => {
 
 export const sendResetPassword = async (email: string) => {
   const emailToken = jwt.sign({ email }, jwtToken, {
-    expiresIn: '1d',
+    expiresIn: "1d",
   });
 
-  const emailResetPasswordLink = `http://localhost:4000/api/auth/resetPassword?token=${emailToken}`;
+  const emailResetPasswordLink = `https://urbannerd-production.up.railway.app//api/auth/resetPassword?token=${emailToken}`;
 
   const htmlTemplatePath = path.resolve(
     __dirname,
-    '../../templates/mail/resetPassword.html',
+    "../../templates/mail/resetPassword.html"
   );
 
-  const htmlTemplate = fs.readFileSync(htmlTemplatePath, 'utf8');
+  const htmlTemplate = fs.readFileSync(htmlTemplatePath, "utf8");
 
   const emailHTML = htmlTemplate.replace(
-    '{{ResetPasswordLink}}',
-    emailResetPasswordLink,
+    "{{ResetPasswordLink}}",
+    emailResetPasswordLink
   );
 
   await transporter.sendMail({
