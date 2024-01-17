2948579a5270c359bdda864ba29afe4accabafa4
BigZoo92 <givernaudenzo@gmail.com>
2024-01-17 12:58:02 +0100

    add get all users

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,78 +1,106 @@
-import express from 'express';
-import { signup, login, confirmSignup, isAuth } from './auth';
-import { editUserInfo, getBookmark, getFollowersCount, getFollowingsCount, toggleFollow, isUserFollowing, getUserInfoWithId } from './user';
+import express from "express";
+import { signup, login, confirmSignup, isAuth } from "./auth";
+import {
+  editUserInfo,
+  getBookmark,
+  getFollowersCount,
+  getFollowingsCount,
+  toggleFollow,
+  isUserFollowing,
+  getUserInfoWithId,
+} from "./user";
 
-import { createPost, getAllPosts, likePost, isPostLikedByUser, toggleBookmark, isPostBookmarkedByUser, getPostLikesCount, getUserLikedPosts, getUserPosts } from './post';
+import {
+  createPost,
+  getAllPosts,
+  likePost,
+  isPostLikedByUser,
+  toggleBookmark,
+  isPostBookmarkedByUser,
+  getPostLikesCount,
+  getUserLikedPosts,
+  getUserPosts,
+} from "./post";
 
-import { upload } from '../constant';
-import { createProduct, getProductWithId, getAllProduct } from './product';
-import { payment } from './payment';
+import { upload } from "../constant";
+import { createProduct, getProductWithId, getAllProduct } from "./product";
+import { payment } from "./payment";
+import { getAllUsers } from "./user/getAllUsers";
 
 const router = express.Router();
 
 // SIGNUP ROUTE
-router.post('/auth/signup', (req, res) => signup(req, res));
-router.get('/auth/confirmSignup', (req, res) => confirmSignup(req, res));
-
-
-// LOGIN ROUTE
-router.post('/checkAuthenticated', (req, res) => isAuth(req, res));
+router.post("/auth/signup", (req, res) => signup(req, res));
+router.get("/auth/confirmSignup", (req, res) => confirmSignup(req, res));
 
 // LOGIN ROUTE
-router.post('/auth/login', (req, res) => login(req, res));
+router.post("/checkAuthenticated", (req, res) => isAuth(req, res));
+
+// LOGIN ROUTE
+router.post("/auth/login", (req, res) => login(req, res));
 
 //POST ROUTE
-router.post('/post', upload,  (req, res) => createPost(req, res));
+router.post("/post", upload, (req, res) => createPost(req, res));
 
 //GET ALL POST
-router.post('/getAllPosts',  (req, res) => getAllPosts(req, res));
+router.post("/getAllPosts", (req, res) => getAllPosts(req, res));
 
 //GET ALL PRODUCT
-router.post('/getAllProduct',  (req, res) => getAllProduct(req, res));
+router.post("/getAllProduct", (req, res) => getAllProduct(req, res));
 
 //EDIT USER INFO
-router.post('/editProfil', upload,  (req, res) => editUserInfo(req, res));
+router.post("/editProfil", upload, (req, res) => editUserInfo(req, res));
 
 //GET USER INFO WITH HIS ID
-router.get('/getUserInfoWithId/:userId',  (req, res) => getUserInfoWithId(req, res));
+router.get("/getUserInfoWithId/:userId", (req, res) =>
+  getUserInfoWithId(req, res)
+);
 
 //GET PRODUCT WITH ID
-router.get('/getProductWithId/:productId',  (req, res) => getProductWithId(req, res));
+router.get("/getProductWithId/:productId", (req, res) =>
+  getProductWithId(req, res)
+);
 
 //LIKE
-router.post('/like',  (req, res) => likePost(req, res));
+router.post("/like", (req, res) => likePost(req, res));
 
 //GET LIKE
-router.get('/isPostLikedByUser/:postId', (req, res) => isPostLikedByUser(req, res));
+router.get("/isPostLikedByUser/:postId", (req, res) =>
+  isPostLikedByUser(req, res)
+);
 
 //BOOKMARK
-router.post('/toggleBookmark', toggleBookmark);
+router.post("/toggleBookmark", toggleBookmark);
 
 //GET BOOKMARK STAT
-router.get('/isPostBookmarkedByUser/:postId', isPostBookmarkedByUser);
+router.get("/isPostBookmarkedByUser/:postId", isPostBookmarkedByUser);
 
 //GET BOOKMARK
-router.get('/bookmark', getBookmark);
+router.get("/bookmark", getBookmark);
 
 // GET LIKE COUNT
-router.get('/post/:postId/likes/count', (req, res) => getPostLikesCount(req, res));
+router.get("/post/:postId/likes/count", (req, res) =>
+  getPostLikesCount(req, res)
+);
 
 // GET LIKED POST BY A USER
-router.get('/user/:userId/likedPosts', (req, res) => getUserLikedPosts(req, res));
+router.get("/user/:userId/likedPosts", (req, res) =>
+  getUserLikedPosts(req, res)
+);
 
 // GET POST BY A USER
-router.get('/user/:userId/posts', (req, res) => getUserPosts(req, res));
+router.get("/user/:userId/posts", (req, res) => getUserPosts(req, res));
 
 // CREAT PRODUCT
-router.post('/product/create', upload, (req, res) => createProduct(req, res));
+router.post("/product/create", upload, (req, res) => createProduct(req, res));
 
 // PAY
-router.post('/paiement', (req, res) => payment(req, res));
+router.post("/paiement", (req, res) => payment(req, res));
 
-router.post('/user/toggleFollow', toggleFollow);
-router.get('/user/:userId/followers/count', getFollowersCount);
-router.get('/user/:userId/followings/count', getFollowingsCount);
-router.get('/user/:followingId/isFollowing', isUserFollowing);
-
+router.post("/user/toggleFollow", toggleFollow);
+router.get("/user/:userId/followers/count", getFollowersCount);
+router.get("/user/:userId/followings/count", getFollowingsCount);
+router.get("/user/:followingId/isFollowing", isUserFollowing);
+router.get("/user/getAllUsers", getAllUsers);
 
 export default router;
---
+++ backend/src/routes/user/getAllUsers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { prisma } from "../..";
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const userIds = await prisma.user.findMany({
+      select: {
+        id: true,
+      },
+    });
+    await prisma.$disconnect();
+    res.status(200).json(userIds);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des users :", error);
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
